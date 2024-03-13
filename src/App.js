fdb485e523c29d915f555514d7639d7a68d434f7
Muheeb-shariff <muheeb.ahmed.shariff@gmail.com>
2024-03-13 21:40:08 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main-container'>
+        <div className='container1'>
+          <div>PORTFOLIO</div>
+          <div className='name'><h1>Muheeb<br/> Ahmed</h1></div>
+          <div className='newCon'>
+          <div>Developer & Consultant</div>
+          <div>Email Me</div>
+          </div>
+          </div>
+        <div className='container2'><img src="assets/2neG8z.jpg"></img></div>
+      </div>
     </div>
   );
 }
